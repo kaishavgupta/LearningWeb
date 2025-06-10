15d575527ea84144fdc3acdbb56ba45aa261a593
kaishavgupta <kaishavgupta4.2001@gmail.com>
2025-06-10 12:58:12 +0530

    02_basics_started

---
+++ Java_Script/js-hiteshNotes/02_basics/arrays.js
@@ -0,0 +1,51 @@
+//array
+
+
+// const arr= [1,2,3,4,5]
+
+//~ arr.push(20)
+// console.log(arr);
+//~ arr.pop()
+
+//~ arr.unshift(10)
+// console.log(arr);
+
+// console.log(arr.length);
+
+
+//~ arr.shift()
+// console.log(arr);
+
+//~ console.log(arr.length);
+
+
+// const arr= [1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+// console.log("A , Original",arr);
+
+//~ const mySlice = arr.slice(1,3) //slice slices start to end-1 also it don't changes original  
+
+// console.log("Slice ",mySlice);
+// console.log("B ,Array after SLICE",arr);
+
+//~ const mysplice=arr.splice(1,3) //splice slices start to end also changes original
+
+// console.log("Splice ",mysplice);
+// console.log("C ,Array after SPLICE",arr);
+
+const arr= [1,2,3,4,5]
+
+//* const myNewArr = new Array("x","y","z")
+//  console.log(myNewArr)
+
+//  console.log(arr.includes(3));
+
+//~ const useJoin =arr.join()
+// console.log(arr);
+// console.log(useJoin)
+// console.log(typeof useJoin);
+
+//~ console.log(arr.includes(3));  //op:true
+//~ console.log(arr.includes(9)); //op:false
+//~ console.log(arr.indexOf(3)); //op:2
+//~ console.log(arr.indexOf(9)); //op:-1
