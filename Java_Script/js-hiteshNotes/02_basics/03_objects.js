4630a073de0294ba3525d9db4fa9105b17ac74fb
kaishavgupta <kaishavgupta4.2001@gmail.com>
2025-06-13 16:32:31 +0530

    Objects Started

--- Java_Script/js-hiteshNotes/02_basics/03_objects.js
+++ Java_Script/js-hiteshNotes/02_basics/03_objects.js
@@ -1,1 +1,33 @@
-// singleton
+/*
+Objects can be declaed in 2 way Literals and Constructors
+*/
+
+// singleton //whenever we create through constructor then we create a singleton object , means its an object 
+
+//object literals
+
+Object.create  //creation of another way of object , it is through constructor method and its only said as singleton constructor
+
+const JsUser = {
+    name: "Kaishav",
+    age:22,
+    "fullname":"Kaishav Gupta",
+    location:"Lucknow",
+    email:"kaishavgupta4.2001@gmail.com",
+    isLoggeIn:false,
+    lastLoginDays:["Monday","Saturday"],
+    1:"one"
+}
+
+//~ To access it we have 2 ways
+/*(1)*/console.log(JsUser.name); //? Through . operator 
+
+/*(2a)*/console.log(JsUser[name]);//? keys here it is string if key is number then its accessible Like jsUser[1]
+
+/*(2b)*/console.log(JsUser["name"]);//? accesing key name as like an array element
+// // /*Important*/ console.log(JsUser."fullnaem")
+/*
+ @Important*/ console.log(JsUser["fullname"]) 
+
+ /*
+@Important */ console.log(JsUser."fullname") // We can't access it like , therefore its been said to access like JsUser["fullname"]
--- Java_Script/js-hiteshNotes/practice/array_string_practice.js
+++ Java_Script/js-hiteshNotes/practice/array_string_practice.js
@@ -1,27 +1,75 @@
-// 🧠 Beginner Level
-// Create an array of numbers from 1 to 10.
+/*
+  🧠 Beginner Level
+  Create an array of numbers from 1 to 10.
+  Add 0 at the start
+  Remove the last element
+  Check if the array includes the number 5
+  Slice the array from index 2 to 5 and print both the slice and the original array. Then splice from index 2 to 4 and print again.
+  Use .join() on an array of colors ["Red", "Green", "Blue"] and convert it into a single comma-separated string.*/
 
-// Add 0 at the start
-
-// Remove the last element
-
-// Check if the array includes the number 5
-
-// Slice the array from index 2 to 5 and print both the slice and the original array. Then splice from index 2 to 4 and print again.
-
-// Use .join() on an array of colors ["Red", "Green", "Blue"] and convert it into a single comma-separated string.
-
-const arr=[1,2,3,4,5,6,7,8,9,10]
-arr.unshift(0);
-console.log(`Add ${0} at start of array ${arr}`);
-const pop =arr.pop()
-console.log(`Pop ${pop} last element from array ${arr}`);
+//? const arr=[1,2,3,4,5,6,7,8,9,10]
+//? arr.unshift(0);
+//? console.log(`Add ${0} at start of array ${arr}`);
+//? const pop =arr.pop()
+//? console.log(`Pop ${pop} last element from array ${arr}`);
 
 
-console.log(`To check that Array include 5 ${arr.includes(5)}`);
-console.log(`Array ${arr} after slice ${arr.slice(2,5)} original Array is ${arr}`);
-console.log(`Array ${arr} after splice ${arr.splice(2,5)} original Array is ${arr}`);
+//? console.log(`To check that Array include 5 ${arr.includes(5)}`);
+//? console.log(`Array ${arr} after slice ${arr.slice(2,5)} original Array is ${arr}`);
+//? console.log(`Array ${arr} after splice ${arr.splice(2,5)} original Array is ${arr}`);
 
-const arrStrings =["Red", "Green", "Blue"] 
-console.log(arrStrings.join());
+//? const arrStrings =["Red", "Green", "Blue"] 
+//? console.log(arrStrings.join());
+
+/*
+🔄 Intermediate Level
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+Merge both arrays using concat()
+Merge them using spread ...
+Push arr2 inside arr1 as a single element (like you did earlier)
+Flatten the following array completely:
+const messyArray = [1, 2, [3, 4], [5, [6, [7]]]];
+Use Array.from() to:
+Convert "JavaScript" into an array of characters
+Convert {name: "Kaishav"} into an array — observe and explain what happens
+*/
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+console.log(`concatinating arr1 ${arr1} and arr2 ${arr2} using concat is ${arr1.concat(arr2)} `);
+console.log(`concatinating arr1 ${arr1} and arr2 ${arr2} using spread operator is ${ [...arr1 , ...arr2]} `);
+// console.log(`Push arr2 to ${arr2.push(arr1)}`);
+
+const messyArray = [1, 2, [3, 4], [5, [6, [7]]]];
+console.log(`Flatten the Mess Array ${messyArray.flat(Infinity)}`);
+console.log(Array.from("JavaScript"));
+console.log(Array.from({name:"Kaishav"}));
+console.log(Array.of(arr1,arr2));
+
+/* 
+💬 String Challenges
+Use string interpolation to print:
+My name is Kaishav and I am 21 years old.
+Use .charAt() and .indexOf() to find:
+The first character of "developer"
+The index of the letter "e"
+
+const dirtyString = "   kaishav -  gupta   ";
+Trim it
+Remove the dash
+
+Split it into words
+Rejoin it as "kaishav gupta"
+*/
+
+console.log(`My name is ${"Kaishav"} and I am ${21} years old`);
+const chara = "developer"
+console.log(`Index 0f first character is ${chara.charAt()}`);
+console.log(`Index 0f ${'e'} character is ${chara.indexOf('e')}`);
+
+const dirtyString = "   kaishav -  gupta   ";
+
+const cleanString = dirtyString.trim().replace("-","").split()
+console.log(cleanString);
 
